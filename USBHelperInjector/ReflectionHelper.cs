2b799109ee7cc3475f3409ade53910b23200ef6a
arcticdiv <arcticdiv@users.noreply.github.com>
2020-05-10 20:07:13 +0200

    More laziness

--- USBHelperInjector/ReflectionHelper.cs
+++ USBHelperInjector/ReflectionHelper.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
-using System.Resources;
 using System.Windows.Forms;
 
 namespace USBHelperInjector
@@ -13,126 +12,69 @@ namespace USBHelperInjector
     {
         private static readonly Assembly assembly = Assembly.Load("WiiU_USB_Helper");
 
-        public static Type[] Types { get; } = assembly.GetTypes();
+        public static readonly Type[] Types = assembly.GetTypes();
 
-        public static Module MainModule { get; } = assembly.GetModule("WiiU_USB_Helper.exe");
+        public static readonly Module MainModule = assembly.GetModule("WiiU_USB_Helper.exe");
 
-        public static MethodInfo EntryPoint { get; } = assembly.EntryPoint;
+        public static readonly MethodInfo EntryPoint = assembly.EntryPoint;
 
-        public static Type Settings
-        {
-            get
-            {
-                return assembly.GetType("WIIU_Downloader.Properties.Settings");
-            }
-        }
+        public static readonly Type Settings = assembly.GetType("WIIU_Downloader.Properties.Settings");
 
         public static class NusGrabberForm
         {
-            private static Type _type;
-            public static Type Type
-            {
-                get
-                {
-                    if (_type == null)
-                    {
-                        _type = (from type in Types
-                                 where typeof(Form).IsAssignableFrom(type)
-                                 from prop in type.GetProperties(BindingFlags.NonPublic | BindingFlags.Instance)
-                                 where prop.Name == "Proxy"
-                                 select type).FirstOrDefault();
-                    }
-                    return _type;
-                }
-            }
+            private static readonly Lazy<Type> _type = new Lazy<Type>(
+                () => (from type in Types
+                       where typeof(Form).IsAssignableFrom(type)
+                       from prop in type.GetProperties(BindingFlags.NonPublic | BindingFlags.Instance)
+                       where prop.Name == "Proxy"
+                       select type).FirstOrDefault()
+            );
+            public static Type Type => _type.Value;
 
-            private static ConstructorInfo _constructor;
-            public static ConstructorInfo Constructor
-            {
-                get
-                {
-                    if (_constructor == null)
-                    {
-                        _constructor = Type.GetConstructor(Type.EmptyTypes);
-                    }
-                    return _constructor;
-                }
-            }
+            private static readonly Lazy<ConstructorInfo> _constructor = new Lazy<ConstructorInfo>(
+                () => Type.GetConstructor(Type.EmptyTypes)
+            );
+            public static ConstructorInfo Constructor => _constructor.Value;
         }
 
-        public static Type CommonResources
-        {
-            get
-            {
-                return (from type in assembly.GetTypes()
-                        from field in type.GetFields(BindingFlags.NonPublic | BindingFlags.Static)
-                        where field.FieldType == typeof(ResourceManager)
-                        select type).FirstOrDefault();
-            }
-        }
-
-        public static Type FrmCloudSaving
-        {
-            get
-            {
-                return (from type in Types
-                        where typeof(Form).IsAssignableFrom(type)
-                           && type.GetFields(BindingFlags.NonPublic | BindingFlags.Instance)
-                                  .Any(f => f.FieldType == TelerikUI.RadToggleSwitch)
-                        select type).FirstOrDefault();
-            }
-        }
+        private static readonly Lazy<Type> _frmCloudSaving = new Lazy<Type>(
+            () => (from type in Types
+                   where typeof(Form).IsAssignableFrom(type)
+                      && type.GetFields(BindingFlags.NonPublic | BindingFlags.Instance)
+                             .Any(f => f.FieldType == TelerikUI.RadToggleSwitch)
+                   select type).FirstOrDefault()
+        );
+        public static Type FrmCloudSaving => _frmCloudSaving.Value;
 
         public static class FrmAskTicket
         {
-            private static Type _type;
-            public static Type Type
-            {
-                get
-                {
-                    if (_type == null)
-                    {
-                        _type = (from type in Types
-                                 where type.GetProperty("FileLocationWiiU") != null
-                                 select type).FirstOrDefault();
-                    }
-                    return _type;
-                }
-            }
+            private static readonly Lazy<Type> _type = new Lazy<Type>(
+                () => (from type in Types
+                       where type.GetProperty("FileLocationWiiU") != null
+                       select type).FirstOrDefault()
+            );
 
-            private static MethodInfo _okButtonHandler;
-            public static MethodInfo OkButtonHandler
-            {
-                get
-                {
-                    if (_okButtonHandler == null)
-                    {
-                        _okButtonHandler = (from method in Type.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.DeclaredOnly)
-                                            let instructions = PatchProcessor.GetOriginalInstructions(method, out _)
-                                            where instructions.Any(x => x.opcode == OpCodes.Call && ((MethodInfo)x.operand).Name == "set_FileLocation3DS")
-                                            && instructions.Any(x => x.opcode == OpCodes.Ldfld && ((FieldInfo)x.operand).FieldType == TelerikUI.RadTextBox)
-                                            select method).FirstOrDefault();
-                    }
-                    return _okButtonHandler;
-                }
-            }
+            public static Type Type => _type.Value;
 
-            private static List<FieldInfo> _textBoxes;
-            public static List<FieldInfo> TextBoxes
-            {
-                get
-                {
-                    if (_textBoxes == null)
-                    {
-                        _textBoxes = (from instruction in PatchProcessor.GetOriginalInstructions(OkButtonHandler, out _)
-                                      where instruction.opcode == OpCodes.Ldfld
-                                      let field = (FieldInfo)instruction.operand
-                                      where field.FieldType == TelerikUI.RadTextBox
-                                      select field).ToList();
-                    }
-                    return _textBoxes;
-                }
-            }
+            private static readonly Lazy<MethodInfo> _okButtonHandler = new Lazy<MethodInfo>(
+                () => (from method in Type.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.DeclaredOnly)
+                       let instructions = PatchProcessor.GetOriginalInstructions(method, out _)
+                       where instructions.Any(x => x.opcode == OpCodes.Call && ((MethodInfo)x.operand).Name == "set_FileLocation3DS")
+                          && instructions.Any(x => x.opcode == OpCodes.Ldfld && ((FieldInfo)x.operand).FieldType == TelerikUI.RadTextBox)
+                       select method).FirstOrDefault()
+            );
+
+            public static MethodInfo OkButtonHandler => _okButtonHandler.Value;
+
+            private static readonly Lazy<List<FieldInfo>> _textBoxes = new Lazy<List<FieldInfo>>(
+                () => (from instruction in PatchProcessor.GetOriginalInstructions(OkButtonHandler, out _)
+                       where instruction.opcode == OpCodes.Ldfld
+                       let field = (FieldInfo)instruction.operand
+                       where field.FieldType == TelerikUI.RadTextBox
+                       select field).ToList()
+            );
+
+            public static List<FieldInfo> TextBoxes => _textBoxes.Value;
         }
 
 
