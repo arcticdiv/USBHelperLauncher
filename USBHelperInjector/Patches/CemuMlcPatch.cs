cf55e5be30bad575e973f2f998d4cbeae5dfe110
arcticdiv <arcticdiv@users.noreply.github.com>
2021-03-27 19:42:22 +0100

    Remove -mlc launch parameter for Cemu

---
+++ USBHelperInjector/Patches/CemuMlcPatch.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace USBHelperInjector.Patches
+{
+    [HarmonyPatch]
+    class CemuMlcPatch
+    {
+        static MethodBase TargetMethod()
+        {
+            return ReflectionHelper.MainModule
+                .GetType("NusHelper.Emulators.Cemu")
+                .GetMethod("GenerateArguments", BindingFlags.NonPublic | BindingFlags.Instance);
+        }
+
+        static void Postfix(ref string __result)
+        {
+            __result = Regex.Replace(__result, " -mlc \".*?\"", "");
+        }
+    }
+}
